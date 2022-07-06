34c098e4d73d76ec5054e93061b057e5f6c29d9f
dabhoyajay <dabhoyajay7000@gmail.com>
2022-07-06 17:47:58 +0530

    First Commit

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -1,6 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
+import'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:hotstar/list.dart';
 import 'package:hotstar/secondpage.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hotstar/firstpage.dart';
 
 import 'package:hotstar/main.dart';
 
